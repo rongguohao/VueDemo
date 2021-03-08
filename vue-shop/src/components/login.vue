f422bc0121e838b239e9e1615b5e38356c7ff07e
rongguohao <843468011@qq.com>
2021-03-08 21:01:35 +0800

    1

---
+++ vue-shop/src/components/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-container">
+    <div class="login-box"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import LoginComponent from "../components/login.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "",
+    redirect: "/login"
+  },
+  {
+    path: "/login",
+    component: LoginComponent
+  }
+];
 
 const router = new VueRouter({
   routes
