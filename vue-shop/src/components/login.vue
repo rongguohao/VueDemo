7f500c84bbdc99989b649e91d721ea484fc45454
rongguohao <843468011@qq.com>
2021-03-09 20:01:14 +0800

    1

--- vue-shop/src/components/login.vue
+++ vue-shop/src/components/login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="login-container">
-    <div class="login-box"></div>
+    <div class="login-box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,5 +26,24 @@ export default {};
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  .avatar-box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
 }
 </style>
